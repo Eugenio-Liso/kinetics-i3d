1e75119e7aae45a60f894d50536bcf63d45699fe
Eugenio <eugenio.liso@gmail.com>
2019-08-06 13:13:52 +0200

    [WIP] Changing main and script to adapt to multi-evaluation

--- custom_evaluation.sh
+++ custom_evaluation.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
-
 type_prediction=$1
 imagenet_pretrained=$2
-input_video_rgb_or_flow=$3
-input_video_flow=$4
-
-
-if [[ -z ${input_video_rgb_or_flow} && -z ${input_video_flow} ]]; then
+input_folder_rgb=$3
+input_folder_flow=$4
+if [[ -z ${input_folder_rgb} && -z ${input_folder_flow} ]]; then
     echo "No input path has been set correctly. Exiting..."
 elif [[ ${type_prediction} == 'rgb' ]]; then # Only RGB
-    python evaluate_sample.py --imagenet_pretrained "${imagenet_pretrained}" --eval_type "${type_prediction}" --input_video_rgb "${input_video_rgb_or_flow}"
+    python evaluate_sample.py --imagenet_pretrained "${imagenet_pretrained}" --eval_type "${type_prediction}" --input_folder_rgb "${input_folder_rgb}"
 elif [[ ${type_prediction} == 'flow' ]]; then
-    python evaluate_sample.py --imagenet_pretrained "${imagenet_pretrained}" --eval_type "${type_prediction}" --input_video_flow "${input_video_rgb_or_flow}"
-else
+    python evaluate_sample.py --imagenet_pretrained "${imagenet_pretrained}" --eval_type "${type_prediction}" --input_folder_flow "${input_folder_rgb}"
+elif [[ ${type_prediction} == 'joint' ]]; then
     #RGB + FLOW
-    python evaluate_sample.py --imagenet_pretrained "${imagenet_pretrained}" --eval_type "${type_prediction}" --input_video_rgb "${input_video_rgb_or_flow}" --input_video_flow "${input_video_flow}"
+    python evaluate_sample.py --imagenet_pretrained "${imagenet_pretrained}" --eval_type "${type_prediction}" --input_folder_rgb "${input_folder_rgb}" --input_folder_flow "${input_folder_flow}"
+else
+  echo "Type of prediction: ${type_prediction} is not supported"
 fi
 #python evaluate_sample.py --imagenet_pretrained true --eval_type rgb --input_video_rgb data/customVideoRGB.npy > out/customVideo_rgb_logs.txt
 #python evaluate_sample.py --imagenet_pretrained true --eval_type flow --input_video_flow data/customVideoFlow.npy > out/customVideo_flow_logs.txt
 #python evaluate_sample.py --imagenet_pretrained true --eval_type joint --input_video_rgb data/customVideoRGB.npy --input_video_flow data/customVideoFlow.npy > out/customVideo_joint_logs.txt
-
 # Sample test from devs
-# python evaluate_sample.py --imagenet_pretrained ${imagenet_pretrained} --eval_type ${type_prediction} --input_video_rgb "${input_video_rgb_or_flow}" --input_video_flow data/v_CricketShot_g04_c01_flow.npy > out/sampleTestVideo_joint_logs.txt
+# python evaluate_sample.py --imagenet_pretrained ${imagenet_pretrained} --eval_type ${type_prediction} --input_video_rgb "${input_video_rgb_or_flow}" --input_video_flow data/CricketShot.npy > out/sampleTestVideo_joint_logs.txt
--- evaluate_sample.py
+++ evaluate_sample.py
@@ -1,5 +1,4 @@
 #!python
-
 # Copyright 2017 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,29 +14,22 @@
 # limitations under the License.
 # ============================================================================
 """Loads a sample video and classifies using a trained Kinetics checkpoint."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
 import os
 import time
-
 import numpy as np
 import tensorflow as tf
-
 import i3d
 from logging_utils import logger_factory as lf
 
 logger = lf.getBasicLogger(os.path.basename(__file__))
-
 _IMAGE_SIZE = 224
-
 # _SAMPLE_PATHS = {
-#     'rgb': 'data/v_CricketShot_g04_c01_rgb.npy',
-#     'flow': 'data/v_CricketShot_g04_c01_flow.npy',
+#     'rgb': 'data/CricketShot.npy',
+#     'flow': 'data/CricketShot.npy',
 # }
-
 _CHECKPOINT_PATHS = {
     'rgb': 'data/checkpoints/rgb_scratch/model.ckpt',
     'rgb600': 'data/checkpoints/rgb_scratch_kin600/model.ckpt',
@@ -45,50 +37,67 @@ _CHECKPOINT_PATHS = {
     'rgb_imagenet': 'data/checkpoints/rgb_imagenet/model.ckpt',
     'flow_imagenet': 'data/checkpoints/flow_imagenet/model.ckpt',
 }
-
 _LABEL_MAP_PATH = 'data/label_map.txt'
 _LABEL_MAP_PATH_600 = 'data/label_map_600.txt'
-
 FLAGS = tf.flags.FLAGS
-
 tf.flags.DEFINE_string('eval_type', '', 'rgb, rgb600, flow, or joint')
 tf.flags.DEFINE_boolean('imagenet_pretrained', True, '')
-tf.flags.DEFINE_string('input_video_rgb', '', 'Input video, as .npy, with RGB format')
-tf.flags.DEFINE_string('input_video_flow', '', 'Input video, as .npy, with Flow format')
+tf.flags.DEFINE_string('input_folder_rgb', '', 'Input folder of videos, as .npy, with RGB format')
+tf.flags.DEFINE_string('input_folder_flow', '', 'Input folder of videos, as .npy, with Flow format')
 
 
 # np.set_printoptions(threshold=np.inf)
-
 def main(unused_argv):
     tf.logging.set_verbosity(tf.logging.INFO)
     eval_type = FLAGS.eval_type
-
     imagenet_pretrained = FLAGS.imagenet_pretrained
+    input_rgb_video_folder = FLAGS.input_folder_rgb
+    input_flow_video_folder = FLAGS.input_folder_flow
+    executions_times_with_video_name = []
 
-    input_video_rgb = FLAGS.input_video_rgb
-    input_video_flow = FLAGS.input_video_flow
+    if input_rgb_video_folder:
+        for filename_rgb in os.listdir(input_rgb_video_folder):
+            video_name = filename_rgb.split(".")[0]
+            complete_filename_rgb = os.path.join(input_rgb_video_folder, filename_rgb)
+            if input_flow_video_folder:
+                foundFile = False
+                complete_filename_flow = ""
+                for filename_flow in os.listdir(input_flow_video_folder):
+                    if filename_flow.startswith(video_name):
+                        foundFile = True
+                        complete_filename_flow = os.path.join(input_flow_video_folder, filename_flow)
+                        break
+                    else:
+                        continue
+                if not foundFile:
+                    raise ValueError("A file with the same name must exists in the two folders")
+                else:
+                    execution_time_with_video_name = prediction_phase(eval_type, imagenet_pretrained,
+                                                                      complete_filename_rgb,
+                                                                      complete_filename_flow, video_name)
+            else:
+                execution_time_with_video_name = prediction_phase(eval_type, imagenet_pretrained, complete_filename_rgb, "",
+                                                                  video_name)
+            executions_times_with_video_name.append(execution_time_with_video_name)
+    elif input_flow_video_folder:
+        for filename in os.listdir(input_rgb_video_folder):
+            video_name = filename.split(".")[0]
+            complete_filename_flow = os.path.join(input_rgb_video_folder, filename)
+            execution_time_with_video_name = prediction_phase(eval_type, imagenet_pretrained, "", complete_filename_flow,
+                                                              video_name)
+            executions_times_with_video_name.append(execution_time_with_video_name)
+    else:
+        raise ValueError("Must specify one folder between RGB and flow at least")
+    print(executions_times_with_video_name)
+    # input_video_rgb = FLAGS.input_video_rgb
+    # input_video_flow = FLAGS.input_video_flow
 
+
+def prediction_phase(eval_type, imagenet_pretrained, input_video_rgb, input_video_flow, video_name):
     if eval_type not in ['rgb', 'rgb600', 'flow', 'joint']:
         raise ValueError('Bad `eval_type`, must be one of rgb, rgb600, flow, joint')
 
     logger.info(f'Chosen evaluation type: {eval_type}')
-
-    # TODO: Check other input types or better exception handling
-    if eval_type == 'joint':  # Joint is the default value, it mixes the flows
-        # if input_video_rgb == '' and input_video_flow == '':
-        #     logger.info("Going to use, as RGB and Flow input, a default test video.")
-        #     logger.info(_SAMPLE_PATHS)
-        if input_video_rgb == '' or input_video_flow == '':
-            raise ValueError('Must specify both flows.')
-    elif (eval_type == 'rgb' or eval_type == 'rgb600') and input_video_rgb == '':
-        raise ValueError("Going to use, as RGB input, a default test video.")
-        # logger.info(_SAMPLE_PATHS['rgb'])
-        # input_video_rgb = _SAMPLE_PATHS['rgb']
-    elif eval_type == 'flow' and input_video_flow == '':
-        raise ValueError('You have not specified both input flows. Using default test video.')
-        # logger.info(_SAMPLE_PATHS['flow'])
-        # input_video_flow = _SAMPLE_PATHS['flow']
-
     logger.info(f"Model inputs: RGB -> {input_video_rgb} | Flow -> {input_video_flow}")
 
     rgb_sample = None
@@ -191,7 +200,10 @@ def main(unused_argv):
     else:
         raise ValueError("The number of frames is undefined")
 
+    execution_times_with_video_name = []
     maximumFrames = False
+    exec_times_with_segments = []
+
     while sliceIndex < totalNumOfFrames:
         nextSliceIndex = sliceIndex + numOfBatchFrames
         print(f"Current sliceIndex: {sliceIndex}-{nextSliceIndex}")
@@ -248,7 +260,12 @@ def main(unused_argv):
                 [model_logits, model_predictions],
                 feed_dict=feed_dict)
             end_time = time.time()
-            logger.info("--- Execution time: %s seconds ---" % (end_time - start_time))
+            exec_time = end_time - start_time
+
+            logger.info("--- Execution time: %s seconds ---" % exec_time)
+            segment = [(sliceIndex + 1), nextSliceIndex]
+            exec_time = exec_time
+            exec_times_with_segments.append((segment, exec_time))
 
             out_logits = out_logits[0]
             out_predictions = out_predictions[0]
@@ -262,6 +279,10 @@ def main(unused_argv):
                                                                                               kinetics_classes[index]))
 
         sliceIndex = nextSliceIndex
+    execution_time_with_video_name = {
+        video_name: exec_times_with_segments
+    }
+    return execution_time_with_video_name
 
 
 if __name__ == '__main__':
